d5e668b618a98fdc985c2d52a87379d7d1a1d0c0
Snaflus <casper.riis.lie.nielsen@gmail.com>
2022-10-10 10:34:15 +0200

    Basic movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float movementSpeed = 2f;
+
+    public Rigidbody2D mybody;
+
+    private Vector2 movement;
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        mybody.MovePosition(mybody.position + movement * movementSpeed * Time.fixedDeltaTime);
     }
 }
