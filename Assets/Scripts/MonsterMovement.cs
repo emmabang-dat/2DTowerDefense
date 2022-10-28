ddbf6823292e7e0d8476b142098f5b2940776c1d
Snaflus <casper.riis.lie.nielsen@gmail.com>
2022-10-28 12:52:45 +0200

    Waypoints proof of concept

---
+++ Assets/Scripts/MonsterMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMovement : MonoBehaviour
+{
+    public float Speed;
+    private Waypoints waypoints;
+
+    private int waypointIndex;
+
+    void Start()
+    {
+        waypoints = GameObject.FindGameObjectWithTag("Waypoints").GetComponent<Waypoints>();
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, waypoints.waypoints[waypointIndex].position, Speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, waypoints.waypoints[waypointIndex].position) < 0.1f)
+        {
+            waypointIndex++;
+        }
+    }
+}
---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    public Transform[] waypoints;
+
+}
+
