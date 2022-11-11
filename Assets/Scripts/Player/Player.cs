5d2176036cb1767bdf404a4d9b06e0406cac603f
Malthe Thomsen <malthe3@outlook.com>
2022-11-11 11:57:29 +0100

    HealthBar

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,35 +1,38 @@
-﻿using System.Collections;
+﻿using
+	System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
 
-	public int maxHealth = 100;
-	public int currentHealth;
+	
+ //   public int maxHealth = 100;
+	//public int currentHealth;
 
-	public HealthBar healthBar;
+	//public HealthBar healthBar;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-		currentHealth = maxHealth;
-		healthBar.SetMaxHealth(maxHealth);
-    }
+ //   // Start is called before the first frame update
+ //   void Start()
+ //   {
+	//	currentHealth = maxHealth;
+	//	healthBar.SetMaxHealth(maxHealth);
+ //   }
 
-    // Update is called once per frame
-    void Update()
-    {
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			TakeDamage(20);
-		}
-    }
+ //   // Update is called once per frame
+ //   void Update()
+ //   {
+	//	if (Input.GetKeyDown(KeyCode.Space))
+	//	{
+	//		TakeDamage(20);
+	//	}
+ //   }
 
-	void TakeDamage(int damage)
-	{
-		currentHealth -= damage;
+	//void TakeDamage(int damage)
+	//{
+	//	currentHealth -= damage;
 
-		healthBar.SetHealth(currentHealth);
-	}
+	//	healthBar.SetHealth(currentHealth);
+	//}
+	
 }
